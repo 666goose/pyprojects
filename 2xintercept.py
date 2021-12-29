e227b96b21438e0fc5ee809ba7e32f377bb6f6e9
666goose <96848962+666goose@users.noreply.github.com>
2021-12-29 18:33:22 -0500

    Add files via upload

---
+++ 2xintercept.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 13:53:39 2021
+
+@author: gperr
+"""
+print("for y=mx+b enter numbers in the form: m b ")
+print("and python Returns the value of x intercept with the x axis")
+
+input_number = input().split()
+#print("[m, b]")
+#print(input_number)
+
+n=int(len(input_number))
+
+if n%2 == 0 :ArithmeticError 
+    
+else :
+    print("Error: missing values in input")
+count = 0
+paircount = 0
+while (count < n):
+    m = input_number[0+int(count)]
+    b = input_number[1+int(count)]
+    count= count + 2
+    paircount = paircount + 1
+    x = -float(b)/float(m)
+    
+    print(paircount)
+    
+    print("y=0 : x="),print(x)
